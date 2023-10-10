c4ebb327159c0102d61dd0dd5ded742c042419a3
nashdev255 <yutori1107@gmail.com>
2023-10-10 23:46:41 +0900

    fixed : __construct

--- htdocs/user.php
+++ htdocs/user.php
@@ -3,13 +3,13 @@
     public $name;
     public $text;
 
-    public function __constructer($name, $text) {
+    public function __construct($name, $text) {
       $this->name = $name;
       $this->text = $text;
     }
 
     public function printUserData() {
-      echo "名前 : {$this->name}";
+      echo "名前 : {$this->name}<br />";
       echo "自己紹介 : {$this->text}";
     }
   }
